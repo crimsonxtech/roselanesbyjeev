4d792d913d41fafa8835736c379d713bab7bb8e9
crimsonxtech <crimsonxtech@gmail.com>
2026-09-19 11:40:04 +0530

    removed2

--- app/quote-confirmation/page.tsx
+++ app/quote-confirmation/page.tsx
@@ -564,7 +564,7 @@ export default function QuoteConfirmationPage() {
           client: {
             name: "laxmi",
             phone: "9347991270",
-            email: "crimsonsayss@gmail.com",
+            email: "laxmiyelimela@gmail.com",
             budget: "3.5 lakhs - 5 lakhs",
           },
           events,
